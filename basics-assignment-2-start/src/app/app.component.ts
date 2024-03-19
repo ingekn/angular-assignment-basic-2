6d02a4e35d061ec485d30aed118570f9a8b33761
ingekn <133350400+ingekn@users.noreply.github.com>
2024-03-19 17:49:54 +0000

    Added data two way binding

--- basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  userName = '';
+  resetButton(event) {
+    this.userName = event.target.value;
+  }
 }
