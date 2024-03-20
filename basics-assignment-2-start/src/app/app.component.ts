fc2fd6f633037e5c3e64c20cd4fd702cd421e7d6
ingekn <133350400+ingekn@users.noreply.github.com>
2024-03-20 21:46:56 +0000

    solved first data binding assignment

--- basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   userName = '';
-  resetButton(event) {
-    this.userName = event.target.value;
-  }
 }
